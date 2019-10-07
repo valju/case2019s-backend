fc48fde0bfc54b5a8192fd193a5a196c6a7f23ad
Välimäki Juhani <valju@haaga-helia.fi>
2019-10-07 10:41:43 +0300

    Temp delete autoPush.bat

--- autoPush.bat
+++
@@ -1,8 +0,0 @@
-echo off
-echo "Automatic push to remote master, with hard-coded commit message"
-git add -A
-git commit -m "Auto push while e.g. editing Dia file. Do not do this at home."
-git push
-echo "Auto push done(?)"
-echo on
-pause
