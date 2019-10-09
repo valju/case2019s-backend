f193188674e45e8b2473803dcf8fa341dad50720
duongthienlee <duongthienlee@gmail.com>
2019-10-09 16:42:30 +0300

    fixed(SQL_Scripts: UrgencyType scripts): Fixed UrgencyType id value, sqlscript formatted. Details: Fixed id value for UrgencyType(10 was supposed to be the highest urgency, 30 is the low one) Code formatting for  create_table scripts (UrgencyType table)

--- Database/SQL_Scripts/01_create_tables.sql
+++ Database/SQL_Scripts/01_create_tables.sql
@@ -5,6 +5,7 @@ CREATE TABLE UrgencyType (
     id INTEGER NOT NULL,
     name VARCHAR(200) NOT NULL UNIQUE,
     description VARCHAR(20000),
+
     CONSTRAINT PK_UrgencyType PRIMARY KEY (id)
 ) ENGINE = InnoDB;
 
--- Database/SQL_Scripts/02_insert_test_data.sql
+++ Database/SQL_Scripts/02_insert_test_data.sql
@@ -4,9 +4,9 @@
 INSERT INTO
   UrgencyType (id, name, description)
 VALUES
-  (10, 'low', 'The value is low'),
+  (30, 'low', 'The value is low'),
   (20, 'medium', 'The value is medium'),
-  (30, 'high', 'The value is high');
+  (10, 'high', 'The value is high');
 
 -- EventType
 
