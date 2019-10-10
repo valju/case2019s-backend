dd77ffabcf718b8fbdceb5e5382573ecd03555bf
Felix Bloch <felix.bloch@myy.haaga-helia.fi>
2019-10-10 13:26:38 +0300

    Formatted and added the SQL scripts for Svetlana and Giang.

--- Database/SQL_Scripts/03_test_queries.sql
+++ Database/SQL_Scripts/03_test_queries.sql
@@ -3,8 +3,20 @@
 -- Event
 
 -- AreaUser
+SELECT * FROM AreaUser ORDER BY areaID ASC;
+
+SELECT COUNT (DISTINCT areaID) FROM AreaUser;
 
 -- User
+SELECT * FROM User ORDER BY id DESC;
+
+SELECT firstName FROM User WHERE firstName LIKE 'b%';
+
+SELECT * FROM User WHERE id IN (SELECT userId FROM AreaUser);
+
+SELECT lastName FROM User WHERE lastName LIKE '%a';
+
+SELECT email from User WHERE email LIKE '%gmail.com';
 
 -- Location
 
@@ -13,6 +25,8 @@
 -- LocationType
 
 -- EventType
+DESCRIBE EventType;
+SELECT * FROM EventType;
 
 -- UrgencyType
 DESCRIBE UrgencyType;
