5c5465d273feca64d429533d41df979fbaf79d37
vehuo <vesa.huotarinen@gmail.com>
2019-11-06 10:20:22 +0200

    Updated DB EventTypes, created a new EventTypes list service

---
+++ src/routes/api/eventType.js
@@ -0,0 +1,17 @@
+import express from "express";
+import knex from "../../db/index";
+
+const eventType = express.Router();
+
+// GET
+/** http://localhost:8989/api/eventType/all  with method=GET **/
+
+eventType.get("/all", function (req, res) {
+  knex
+    .select("name", "description", "defaultUrgency")
+    .from("EventType")
+    .then(data => res.status(200).json(data))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
+export default eventType;
--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 
 import urgencyType from './urgencyType'
+import eventType from "./eventType";
 
 const routes = express.Router();
 routes.use('/urgencyType', urgencyType);
+routes.use("/eventType", eventType);
 
 export default routes;
