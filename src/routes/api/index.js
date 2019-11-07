a742b3020a40e56a89d0427b4077885ac26bbd02
Amber  Karki <amberkarki@Ambers-MacBook-Pro.local>
2019-11-07 10:23:05 +0200

    creating users.js and API services-amber

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -2,9 +2,11 @@ import express from "express";
 
 import urgencyType from './urgencyType'
 import eventType from "./eventType";
+import users from "./users";
 
 const routes = express.Router();
 routes.use('/urgencyType', urgencyType);
 routes.use("/eventType", eventType);
+routes.use("/users", users);
 
 export default routes;
