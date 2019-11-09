2d5eced96dd0079d79a62819adf4f05330a023f8
Felix Bloch <felix.bloch@myy.haaga-helia.fi>
2019-11-09 20:04:06 +0200

    I formatted the code to be to our standard and cleaned some unecessary whitespaces, etc.

--- src/routes/api/locationType.js
+++ src/routes/api/locationType.js
@@ -3,12 +3,12 @@ import knex from "../../db/index";
 
 const locationType = express.Router();
 
-locationType.get("/all",function(req,res){
-    knex
+locationType.get("/all", function (req, res) {
+  knex
     .select()
     .from("LocationType")
     .then(data => res.status(200).json(data))
-    .catch(err =>res.status(500).json({err:err.message}));
+    .catch(err => res.status(500).json({ err: err.message }));
 });
 
 export default locationType;
