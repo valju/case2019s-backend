4f919b8a4b933f7c045551f42e19cfde7f0ed681
Felix Bloch <felix.bloch@myy.haaga-helia.fi>
2019-11-19 10:59:22 +0200

    I added the location getById and delete API's ad test cases.

--- src/routes/api/location.js
+++ src/routes/api/location.js
@@ -3,12 +3,63 @@ import knex from "../../db/index";
 
 const location = express.Router();
 
+
+// GET ALL
 location.get("/all", function (req, res) {
-  knex
-    .select()
-    .from("Location")
+  knex("Location")
     .then(data => res.status(200).json(data))
     .catch(err => res.status(500).json({ err: err.message }));
+});
+
+// GET ONE
+location.get("/:id", function (req, res) {
+  knex("Location")
+    .where("id", req.params.id)
+    .then(data => {
+      if (data.length == 0) {
+        res
+          .status(404)
+          .send("Invalid row number: " + req.params.id)
+          .end();
+      } else {
+        res
+          .status(200)
+          .send(data)
+          .end();
+      }
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .send("Database error: " + error.errno)
+        .end();
+    });
+});
+
+// DELETE ONE
+location.delete("/:id", function (req, res){
+  knex("Location")
+  .where("id", req.params.id)
+  .del()
+  .then(data => {
+    if (data == 0) {
+      res
+        .status(404)
+        .send("Invalid row number: " + req.params.id)
+        .end();
+    } else {
+      res
+        .status(200)
+        .send("Delete successful! Count of deleted rows: " + data)
+        .end();
+    }
+  })
+  .catch(error => {
+    res
+      .status(500)
+      .send("Database error: " + error.errno)
+      .end();
+  });
 });
 
 export default location;
